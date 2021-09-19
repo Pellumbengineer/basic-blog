23fbbde55fa8c99997c5ca4e122c0f72990f819b
Pellumb Baboci <pellumbbaboci@gmail.com>
2021-09-19 22:41:14 +0200

    change of style of notfound page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div className='shadow-xl rounded-xl px-3 py-3 m-5 mx-auto'>
+    <div className='shadow-xl rounded-xl px-3 py-3 m-5 mx-auto '>
       <h1 className='text-center font-semibold'>Sorry</h1>
       <p className='text-center mt-3'>That page can not be found</p>
       <Link to='/'>
-        <button className='mt-5 mx-auto bg-red-700 text-white p-2 rounded-md hover:bg-red-500'>
+        <button className='mt-5 m-auto w-full bg-red-700 text-white p-2 rounded-md hover:bg-red-500'>
           Go Home
         </button>
       </Link>
